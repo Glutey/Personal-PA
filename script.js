9671ac533bf5407b1561dda678f7fe76221db4af
Glutey <mr_tscreen@hotmail.com>
2023-01-31 23:12:57 +0000

    JS code updated along with HTML

--- script.js
+++ script.js
@@ -1,60 +1,47 @@
 let dateTime = moment().format("ddd, D, MMM, YYYY, HH:mm:ss");
 $("#time-display").text(dateTime);
 
-$(function () {
-    $('.hour-row').sortable({
-      placeholder: 'ui-state-highlight',
-    });
-    // $('.hour-row').disableSelection();
-  });
-  
+let timeBlocks = [
+  $('#nine-am'),
+  $('#ten-am'),
+  $('#eleven-am'),
+  $('#twelve-pm'),
+  $('#one-pm'),
+  $('#two-pm'),
+  $('#three-pm'),
+  $('#four-pm'),
+  $('#five-pm')
+];
 
-const timeblocks = [
-    { start: '01:00:00', end: '01:59:59' },
-    { start: '02:00:00', end: '02:59:59' },
-    { start: '03:00:00', end: '03:59:59' },
-    { start: '04:00:00', end: '04:59:59' },
-    { start: '05:00:00', end: '05:59:59' },
-    { start: '06:00:00', end: '06:59:59' },
-    { start: '07:00:00', end: '07:59:59' },
-    { start: '08:00:00', end: '08:59:59' },
-    { start: '09:00:00', end: '09:59:59' },
-    { start: '10:00:00', end: '10:59:59' },
-    { start: '11:00:00', end: '11:59:59' },
-    { start: '12:00:00', end: '12:59:59' },
-    { start: '13:00:00', end: '13:59:59' },
-    { start: '14:00:00', end: '14:59:59' },
-    { start: '15:00:00', end: '15:59:59' },
-    { start: '16:00:00', end: '16:59:59' },
-    { start: '17:00:00', end: '17:59:59' },
-    { start: '18:00:00', end: '18:59:59' },
-    { start: '19:00:00', end: '19:59:59' },
-    { start: '20:00:00', end: '20:59:59' },
-    { start: '21:00:00', end: '21:59:59' },
-    { start: '22:00:00', end: '22:59:59' },
-    { start: '23:00:00', end: '23:59:59' },
-    { start: '00:00:00', end: '00:59:59' }
-    
-  ];
+var now = moment().format('HH');
+
+for (var i = 0; i < timeBlocks.length; i++){
+  var timeBlock = timeBlocks[i];
+  var hour = timeBlock.attr('hour');
+  if (hour < now) {
+    timeBlock.find('textarea').addClass('past');
+  } 
+  if (hour === now) {
+    timeBlock.find('textarea').addClass('present');
+  } 
+  if (hour > now) {
+    timeBlock.find('textarea').addClass('future');
+  } 
+}
+
+function saveText(timeBlock, eventText){
+  localStorage.setItem(timeBlock, eventText);
+}
+
+for (var i = 0; i < timeBlocks.length; i++){
+  var timeBlock = timeBlocks[i];
+  console.log(timeBlock.find('button')[0])
+  timeBlock.find('button').get(0).addEventListener('click', function(){
+    var slot = timeBlock.attr('hour');
+    var text = timeBlock.find('textarea').val();
+    console.log(slot)
+    console.log(text)
+    // saveText(slot, text);
+  })
   
-  const presentColor = 'green';
-  const pastColor = 'red';
-  const futureColor = 'blue';
-  
-  const now = new Date();
-  
-  timeblocks.forEach(timeblock => {
-    const start = new Date(timeblock.start);
-    const end = new Date(timeblock.end);
-    
-    if (start < now && end > now) {
-      timeblock.color = presentColor;
-    } else if (start < now && end < now) {
-      timeblock.color = pastColor;
-    } else if (start > now && end > now) {
-      timeblock.color = futureColor;
-    }
-  });
-  
-  console.log(timeblocks);
-  
+}
