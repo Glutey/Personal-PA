741974669522e25d12d1a149ea674bd4e9e3b6b2
Glutey <mr_tscreen@hotmail.com>
2023-01-31 23:46:50 +0000

    Added comments to the entire program

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+// moment() allowing me to find the date and time applicable to the user when viewing the site in real time
 let dateTime = moment().format("ddd, D, MMM, YYYY, HH:mm:ss");
 $("#time-display").text(dateTime);
 
+// created this array to allow me to loop through all the timed slots within the scheduler
 let timeBlocks = [
   $('#nine-am'),
   $('#ten-am'),
@@ -15,6 +17,8 @@ let timeBlocks = [
 
 var now = moment().format('HH');
 
+// this for loop will iterate through all of the time slots and apply the correct colouring
+// dependant on the current time and whether or not that slot is in the past, present or future
 for (var i = 0; i < timeBlocks.length; i++){
   var timeBlock = timeBlocks[i];
   var hour = timeBlock.attr('hour');
@@ -29,11 +33,12 @@ for (var i = 0; i < timeBlocks.length; i++){
   } 
 }
 
+// this code was meant to save text to the local storage... i cannot for the life of me figure it out...
 function saveText(timeBlock, eventText){
   // console.log(timeBlock)
   // console.log(eventText)
-  // localStorage.setItem(timeBlock, eventText);
-for (var i = 0; i < timeBlocks.length; i++){
+  localStorage.setItem(timeBlock, eventText);
+  for (var i = 0; i < timeBlocks.length; i++){
   var timeBlock = timeBlocks[i];
   var slot = timeBlock.attr('hour');
   var text = timeBlock.find('textarea').val();
